48f9c3590ef41548a8b42a0d181ba3157ec6f811
hrvojeA <hrvoje.antunovic@edu.fit.ba>
2016-12-19 10:27:11 +0100

    adding settings page

--- admin/class-plugin-name-admin.php
+++ admin/class-plugin-name-admin.php
@@ -100,6 +100,34 @@ class Plugin_Name_Admin {
 
 	}
 
-	
+	public function add_plugin_admin_menu() {
+
+		/*
+         * Add a settings page for this plugin to the Settings menu.
+         *
+         * NOTE:  Alternative menu locations are available via WordPress administration menu functions.
+         *
+         *        Administration Menus: http://codex.wordpress.org/Administration_Menus
+         *
+         */
+		add_options_page( 'Translate24 Dashboard', 'Translate24', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page')
+		);
+	}
+
+	public function add_action_links( $links ) {
+		/*
+        *  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+        */
+		$settings_link = array(
+			'<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+		);
+		return array_merge(  $settings_link, $links );
+
+	}
+	public function display_plugin_setup_page() {
+		include_once( 'partials/plugin-name-admin-display.php' );
+	}
+
+
 
 }
--- includes/class-plugin-name.php
+++ includes/class-plugin-name.php
@@ -154,6 +154,14 @@ class Plugin_Name {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+
+		// Add menu item
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_admin_menu' );
+
+		// Add Settings link to the plugin
+		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
+		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
+
 	}
 
 	/**
@@ -169,6 +177,7 @@ class Plugin_Name {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+
 
 	}
 
